162bc2b8fcf06355912d2c3e2a52783eda1d72f5
yj <js58570508@naver.com>
2022-11-08 13:30:32 +0900

    quest00 완료

--- Quest03/skeleton/quest/quest00/main.js
+++ Quest03/skeleton/quest/quest00/main.js
@@ -25,20 +25,33 @@ var imgText = {
  /*img요소를 생성하여
 썸네일 이미지들을 한번씩 돌면서 경로와 alt값을 넣어주고
 thumbBar의 자식요소로 생성하기
-newImage클릭 시 해당 이미지의 src값을 displayedImage의 src값에 넣어주기*/
+newImage클릭 시 해당 이미지의 src값을 displayedImage의 src값에 넣어주기
+darken버튼과 lighten버튼 기능 만들기-버튼안의 텍스트와 overlay의 backgroundColor 바꿔주기*/
+
+var newImageArr = [];
 for(var i = 0; i < imgArr.length; i++){
    var newImage = document.createElement('img');
     newImage.setAttribute('src', 'images/'+imgArr[i]);
     newImage.setAttribute('alt', imgText['pic'+(i+1)+'.jpg']);
     thumbBar.appendChild(newImage);
-    newImage.addEventListener('click', function(){
+
+    /*displayedImage의 src속성을 바꿔주기 위한 foreach문에 필요한 newImage 배열 생성*/
+    newImageArr.push(newImage);   
+}
+
+/*src변경 함수*/
+function displayed(imgItem){
+    imgItem.addEventListener('click', function(){
         displayedImage.setAttribute('src', this.getAttribute('src'));
     })
-}
+};
+
+newImageArr.forEach(displayed);
+
 
 /* Wiring up the Darken/Lighten button */
 
-/*btn클릭 시 btn 안의 글자가 Darken이면 어둡게, 아니라면 밝게 하는 함수 */
+/*btn클릭 시 btn 안의 글자가 Darken이면 어둡게, 아니라면 밝게 하는 함수 적용 */
 btn.addEventListener('click', function(){
     if(btn.innerHTML == 'Darken'){
         textLight();
