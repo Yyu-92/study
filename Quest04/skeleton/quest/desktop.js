04767195b539dad809178645abfea1af9c1bc2cf
yj <js58570508@naver.com>
2022-11-21 18:14:27 +0900

    바탕화면시스템구현중

--- Quest04/skeleton/quest/desktop.js
+++ Quest04/skeleton/quest/desktop.js
@@ -1,24 +1,3 @@
-class Desktop {
-	/* TODO: Desktop 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
-	constructor(){
-	
-	}
-
-	
-};
-
-class Icon {
-	/* TODO: Icon 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
-};
-
-class Folder {
-	/* TODO: Folder 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
-};
-
-class Window {
-	/* TODO: Window 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
-};
-
 let desktop = document.getElementsByClassName('desktop')[0];
 let folderArr = [];
 let lastX = 0;
@@ -31,8 +10,11 @@ let menuBar = '';
 let tabPlus = '';
 let tabList = '';
 let tabClose = '';
-
-let body = document.querySelector('body');
+let inputArea = '';
+let submitBtn = '';
+let folderWindow = '';
+let closeBtn = '';
+let tabListArr = [];
 
 let header = document.createElement('header');
 header.setAttribute('class', 'header');
@@ -42,209 +24,241 @@ let tabBar = document.createElement('div');
 tabBar.setAttribute('class', 'tab-bar');
 header.append(tabBar);
 
-/** tab 만들기 */
-function setCreatePlusTab(){
-	tabList = document.createElement('a');
-	tabClose = document.createElement('button');
-	tabList.setAttribute('class', 'tablist');
-	tabClose.setAttribute('class', 'tabclose');
-	tabClose.innerText = 'X';
-	tabList.innerText = '유진의 바탕화면👀';
-	//tabList.style.width = '180px';
-	tabBar.append(tabList);
-	tabList.append(tabClose);
-
-	clickClose(tabClose);
-
-	return tabClose;
-}
-
-setCreatePlusTab();
-
-/** tab 플러스 버튼 만들기 */
-function setCreatePlusBtn(){
-	tabPlus = document.createElement('button');
-	tabPlus.setAttribute('class', 'tabplus');
-	tabPlus.innerText = '+'
-	tabBar.append(tabPlus);
-
-	return tabPlus;
-}
-setCreatePlusBtn();
-console.log(tabPlus);
-
-/** 바탕화면 만들기 */
-function setCreateWallpaper(){
-	wallpaper = document.createElement('div');
-	wallpaperInner = document.createElement('div');
-	wallpaper.setAttribute('class', 'wallpaper');
-	wallpaperInner.setAttribute('class', 'wallpaper-inner');
-	desktop.append(wallpaper);
-	wallpaper.append(wallpaperInner);
-
-	return wallpaperInner;
-}
-setCreateWallpaper();
-console.log(wallpaper);
-
-/**  */
-function setCreateMenubar(){
-	menuBar = document.createElement('div');
-	menuBar.setAttribute('class', 'menu-bar');
-	wallpaperInner.append(menuBar);
-
-	return menuBar;
-}
-
-setCreateMenubar();
-console.log(menuBar);
-
-/** tabplustbtn 클릭 시 tab&&바탕화면 생성 */
-tabPlus.addEventListener('click', function(){
-	setCreatePlusTab();
-	setCreateWallpaper();
-	setCreateMenubar();
-	setCreateInput();
-});
-
-
-
-// for(let j = 0; j < tabListArr.lenght; j++){
-// 	(function(j){
-// 		tabListArr[j].addEventListener('click', function(){
-// 			wallpaperArr[j].style.zIndex = 999;
-// 		});
-// 	})(j);
-// }
-// console.log(tabListArr)
-
-/** 생성input 만들기 */
-function setCreateInput(){
-	for(let i = 0; i < 2; i++){
-		let inputArea = document.createElement('input');
-		inputArea.type = 'text';
-		inputArea.class = 'constructor';
-		inputArea.id = (i == 0) ? 'folderConst' : 'iconConst';
-		inputArea.placeholder = (i == 0) ? '폴더 갯수를 입력해주세요.' : '아이콘 갯수를 입력해주세요.';
-
-		let SubmitBtn = document.createElement('input');
-		SubmitBtn.type = 'submit';
-		SubmitBtn.value = '생성';
-		SubmitBtn.id = (i == 0) ? 'folderSubmit' : 'iconSubmit';
-
-		menuBar.append(inputArea);
-		menuBar.append(SubmitBtn);
+class Desktop {
+	/* TODO: Desktop 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+	constructor(indexNum, btn1, btn2){
+		this.indexNum = indexNum;
+		this.btn1 = btn1;
+		this.btn2 = btn2;
+		this.start();
 	}
-}
 
-setCreateInput();
+	start(){
+		this.setCreateTab();
+		this.setCreateWallpaper();
+		this.setCreateMenubar();
+		this.setCreateInput();
+		this.setCreatePlusBtn();
+	}
 
-/** 아이콘 드래그 */
-function movingIcon(selectIcon){
-	selectIcon.addEventListener('mousedown', function(e){
-		e.preventDefault(); 
-		
-		/** 마우스 왼쪽클릭 0 / 오른쪽 2 / 휠 1 */
-		if(e.button !== 0){ 
-			return;
+	setCreateTab(){
+		tabList = document.createElement('a');
+		tabClose = document.createElement('button');
+		tabList.setAttribute('class', 'tablist');
+		tabList.setAttribute('id', 'tablist'+this.indexNum);
+		tabList.innerText = '유진의 바탕화면👀';
+		tabClose.setAttribute('class', 'tabclose');
+		tabClose.innerText = 'X';
+		tabBar.append(tabList);
+		tabList.append(tabClose);
+	
+		tabListArr.push(tabList);
+	}
+
+	setCreatePlusBtn(){
+		tabPlus = document.createElement('button');
+		tabPlus.setAttribute('class', 'tabplus');
+		tabPlus.innerText = '+'
+		tabBar.append(tabPlus);
+	}
+
+	setCreateWallpaper(){
+		wallpaper = document.createElement('a');
+		wallpaperInner = document.createElement('div');
+		wallpaper.setAttribute('class', 'wallpaper');
+		wallpaper.setAttribute('id', 'wallpaper'+this.indexNum);
+		wallpaperInner.setAttribute('class', 'wallpaper-inner');
+		wallpaperInner.setAttribute('id', 'wallpaper-inner'+this.indexNum);
+		desktop.append(wallpaper);
+		wallpaper.append(wallpaperInner);
+	}
+
+	setCreateMenubar(){
+		menuBar = document.createElement('div');
+		menuBar.setAttribute('class', 'menu-bar');
+		wallpaperInner.append(menuBar);
+	}
+
+	setCreateInput(){
+		for(let i = 0; i < 2; i++){
+			inputArea = document.createElement('input');
+			inputArea.type = 'text';
+			inputArea.class = 'constructor';
+			inputArea.id = (i == 0) ? 'folderConst' : 'iconConst';
+			inputArea.placeholder = (i == 0) ? '폴더 갯수를 입력해주세요.' : '아이콘 갯수를 입력해주세요.';
+	
+			submitBtn = document.createElement('input');
+			submitBtn.type = 'submit';
+			submitBtn.value = '생성';
+			submitBtn.id = (i == 0) ? 'folderSubmit' : 'iconSubmit';
+	
+			menuBar.append(inputArea);
+			menuBar.append(submitBtn);
+
 		}
-		
-		startX = e.clientX // 처음 파일 x좌표 - 클릭한 위치값
-		startY = e.clientY // 처음 파일 y좌표 - 클릭한 위치값
-		if(! this){
+	}
+};
 
+class DesktopExtends{
+	constructor(){
+
+	}
+	
+	movingIcon(selectIcon){
+		selectIcon.addEventListener('mousedown', function(e){
+			e.preventDefault(); 
+			
+			/** 마우스 왼쪽클릭 0 / 오른쪽 2 / 휠 1 */
+			if(e.button !== 0){ 
+				return;
+			}
+			
+			startX = e.clientX // 처음 파일 x좌표 - 클릭한 위치값
+			startY = e.clientY // 처음 파일 y좌표 - 클릭한 위치값
+			
+			
+			document.addEventListener('mouseup', onRemoveEvent); // 마우스 클릭을 멈췄을 때, 내부에서 이벤트를 해체하는 함수를 바인딩
+			document.addEventListener('mousemove', onMove); 
+		});
+		
+		/** 클릭을 해제했을 때 객체가 이동이 되면 안되므로 함수들 해제*/
+		function onRemoveEvent() { 
+			selectIcon.style.position = 'fixed';
+			document.removeEventListener('mouseup', onRemoveEvent); 
+			document.removeEventListener('mousemove', onMove); 
+		} 
+		
+		function onMove(e) { 
+			e.preventDefault(); 
+			selectIcon.style.position = 'fixed';
+		
+			lastX = e.clientX - startX; // 이동한 거리 x
+			lastY = e.clientY - startY; // 이동한 거리 y
+			//console.log(lastX, lastY);
+			
+			startX = e.clientX; //원래 위치값도 옮겨줘야함 (3번쨰로 옮기면 2번째로 옮겨진 그 위치가 원래 위치값이 되니까 )
+			startY = e.clientY; 
+	
+			selectIcon.style.left = (selectIcon.offsetLeft + lastX)+"px";
+			selectIcon.style.top = (selectIcon.offsetTop + lastY)+"px";
+			//console.log(startX, startY)
 		}
-		//this.style.position = 'absolute';
-		
-		document.addEventListener('mouseup', onRemoveEvent); // 마우스 클릭을 멈췄을 때, 내부에서 이벤트를 해체하는 함수를 바인딩
-		document.addEventListener('mousemove', onMove); 
-	});
 	
-	/** 클릭을 해제했을 때 객체가 이동이 되면 안되므로 함수들 해제*/
-	function onRemoveEvent() { 
-		selectIcon.style.position = 'fixed';
-		document.removeEventListener('mouseup', onRemoveEvent); 
-		document.removeEventListener('mousemove', onMove); 
-	} 
-	
-	function onMove(e) { 
-		e.preventDefault(); 
-		selectIcon.style.position = 'fixed';
-	
-		lastX = e.clientX - startX; // 이동한 거리 x
-		lastY = e.clientY - startY; // 이동한 거리 y
-		//console.log(lastX, lastY);
-		
-		startX = e.clientX; //원래 위치값도 옮겨줘야함 (3번쨰로 옮기면 2번째로 옮겨진 그 위치가 원래 위치값이 되니까 )
-		startY = e.clientY; 
-
-		selectIcon.style.left = (selectIcon.offsetLeft + lastX)+"px";
-		selectIcon.style.top = (selectIcon.offsetTop + lastY)+"px";
-		//console.log(startX, startY)
 	}
 
 }
 
-
-/** 폴더창닫기 */
-function clickClose(btn){
-	btn.addEventListener('click', function(){
-		this.parentElement.style.display = 'none';	
-	});
-}
-
-
-/** 아이콘생성 */
-function createIcon(name){
-	name.addEventListener('click', function(){
-		folderValue = (name == folderSubmit) ? Number(document.getElementById('folderConst').value) : Number(document.getElementById('iconConst').value);
-		console.log(folderValue);
-		for(let i = 0; i < folderValue; i++){
+class Icon extends DesktopExtends{
+	/* TODO: Icon 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+	constructor(num, indexNum){
+		super();
+		this.num = num;
+		this.indexNum = indexNum;
+	}
+	createIcon(){
+		for(let i = 0; i < this.num; i++){
 			let icon = document.createElement('img');
-			icon.src = (name == folderSubmit) ? 'images/folder.png' : 'images/file.png';
-			icon.alt = (name == folderSubmit) ? 'folder' : 'file';
-			icon.class = icon.alt;
-			wallpaperInner.appendChild(icon);	
-			//icon.after(icon.class);
-
-			folderArr.push(icon);
+			icon.src = 'images/file.png';
+			icon.alt = 'file';
+			icon.setAttribute('class', 'file');
+			document.querySelector('#wallpaper-inner'+this.indexNum).appendChild(icon);	
 		}
-		folderArr.forEach(movingIcon); 
-		folderArr.forEach(doubleClick);
-
-		document.getElementById('folderConst').value = '';
-		document.getElementById('iconConst').value = '';
-
-	});
+	}
 }
-    
-let folderSubmit = document.getElementById('folderSubmit');
-let iconSubmit = document.getElementById('iconSubmit');
 
-createIcon(folderSubmit);
-createIcon(iconSubmit);
+class Folder extends DesktopExtends{
+	/* TODO: Folder 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+	constructor(num, indexNum){
+		super();
+		this.num = num;
+		this.indexNum = indexNum;
+	}
+	createFolder(){
+		for(let i = 0; i < this.num; i++){
+			let icon = document.createElement('img');
+			icon.src = 'images/folder.png';
+			icon.alt = 'folder';
+			icon.setAttribute('class', 'folder');
+			document.querySelector('#wallpaper-inner'+this.indexNum).appendChild(icon);		
+			
+		}
+	}
+};
 
-let folderImg = document.getElementsByTagName('img'); // HTMLCollection
-//let folderArr = [].slice.call(folderImg); // => 배열로 변환
-
-
-/** 아이콘 더블클릭시 폴더창 생성 및 드래그 */
-function doubleClick(aa){
-	aa.addEventListener('dblclick', function(){
-		let folderWindow = document.createElement('div');
-		let closeBtn = document.createElement('button');
-		folderWindow.setAttribute('class', 'folderWindow');
-		folderWindow.innerText = '폴더입니다^^휴;;';
-		wallpaperInner.append(folderWindow);
+class Window{
+	/* TODO: Window 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+	constructor(){
 		
-		closeBtn.setAttribute('class', 'close');
-		closeBtn.innerText = 'X';
-		folderWindow.append(closeBtn);
+	}
 
-		movingIcon(folderWindow);
-		clickClose(closeBtn);	
-	});	
-}
+	doubleClick(a){
+		a.forEach(function(c){
+			c.addEventListener('dblclick', function(){ 
+				folderWindow = document.createElement('div');
+				closeBtn = document.createElement('button');
+				folderWindow.setAttribute('class', 'folderWindow');
+				folderWindow.innerText = '폴더입니다^^휴;;';
+				this.parentElement.append(folderWindow);
+				closeBtn.setAttribute('class', 'close');
+				closeBtn.innerText = 'X';
+				folderWindow.append(closeBtn);
+
+				function movingIcon(selectIcon){
+					selectIcon.addEventListener('mousedown', function(e){
+						e.preventDefault(); 
+						
+						/** 마우스 왼쪽클릭 0 / 오른쪽 2 / 휠 1 */
+						if(e.button !== 0){ 
+							return;
+						}
+						
+						startX = e.clientX // 처음 파일 x좌표 - 클릭한 위치값
+						startY = e.clientY // 처음 파일 y좌표 - 클릭한 위치값
+						
+						
+						document.addEventListener('mouseup', onRemoveEvent); // 마우스 클릭을 멈췄을 때, 내부에서 이벤트를 해체하는 함수를 바인딩
+						document.addEventListener('mousemove', onMove); 
+					});
+					
+					/** 클릭을 해제했을 때 객체가 이동이 되면 안되므로 함수들 해제*/
+					function onRemoveEvent() { 
+						selectIcon.style.position = 'fixed';
+						document.removeEventListener('mouseup', onRemoveEvent); 
+						document.removeEventListener('mousemove', onMove); 
+					} 
+					
+					function onMove(e) { 
+						e.preventDefault(); 
+						selectIcon.style.position = 'fixed';
+					
+						lastX = e.clientX - startX; // 이동한 거리 x
+						lastY = e.clientY - startY; // 이동한 거리 y
+						//console.log(lastX, lastY);
+						
+						startX = e.clientX; //원래 위치값도 옮겨줘야함 (3번쨰로 옮기면 2번째로 옮겨진 그 위치가 원래 위치값이 되니까 )
+						startY = e.clientY; 
+				
+						selectIcon.style.left = (selectIcon.offsetLeft + lastX)+"px";
+						selectIcon.style.top = (selectIcon.offsetTop + lastY)+"px";
+						//console.log(startX, startY)
+					}
+				
+				}
+				function clickClose(btn){
+					btn.addEventListener('click', function(){
+						this.parentElement.style.display = 'none';	
+					});
+				}
+				movingIcon(folderWindow);
+				clickClose(closeBtn);
+			});	
+		});
+	}
+	
+
+};
+
+
 
 
 
