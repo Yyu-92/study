6347b842d1731d838b72555b9360a4a1a219faeb
yj <js58570508@naver.com>
2022-11-28 15:33:26 +0900

    bingo게임 구현중

--- Quest04/skeleton/bingo/desktop.js
+++ Quest04/skeleton/bingo/desktop.js
@@ -10,6 +10,8 @@ class Desktop {
 
 // let randomNum = Math.floor((Math.random()*(26-1))+1);
 // console.log(randomNum);
+
+/** pcBingo 생성 */
 let numList = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
 let pcBingo = [];
 
@@ -24,11 +26,27 @@ document.getElementById('start-btn').addEventListener('click', function(){
 
 let userBingo = [];
 
-document.getElementById('submit-btn').addEventListener('click', function(){
+/** userBingo 생성 */
+function createUserBingo(){
 	for(let i = 0; i < 25; i++){
-		userBingo[i] = document.getElementById('num' + (i + 1)).value;
+		userBingo[i] = document.getElementById('num' + i).value;
 	}
+	
+	console.log("userBingo : " + userBingo);
+}
 
-	console.log(userBingo);
+
+document.getElementById('submit-btn').addEventListener('click', function(){
+	createUserBingo();
+
+	let result = document.querySelector('.result').value;
+	let resultIndex = userBingo.indexOf(result);
+	
+	/** 빙고 지우기 */
+	document.getElementById('num' + resultIndex).value = 'X';
+	document.getElementById('num' + resultIndex).style.fontSize = '55px';
+
+	/** 등록값 초기화 */
+	document.querySelector('.result').value = '';
 });
 
